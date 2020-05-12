8b649d8c8240970c6388b284646529903c7978ac
J3Cs <joeybrook235@gmail.com>
2020-05-12 10:02:20 -0600

    Archivos para el deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB8KPR1d423vKAcKajjOlLo3qscLz-BkDE",
+    authDomain: "loginbd-8ad5c.firebaseio.com",
+    databaseURL: "https://loginbd-8ad5c.firebaseio.com",
+    projectId: "loginbd-8ad5c",
+    storageBucket: "loginbd-8ad5c.appspot.com"
+  }
 };
